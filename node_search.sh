df16de1daa65f43139b3c57e95a301d64c9d4745
CodyKank <cody.kankel@gmail.com>
2016-12-05 20:59:07 -0500

    Fixed node_search.sh
    Accidentally pushed an out of date copy. Uploaded corrected version. So
    in theory without having the python 3 module running ./node_search.sh
    will allow the node_search.py to run.

--- node_search.sh
+++ node_search.sh
@@ -5,6 +5,9 @@
 # Please read that script or view the man page for more information on what that script does.
 
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/afs/crc.nd.edu/x86_64_linux/python/3.4.0/gcc-4.8.0/lib/
+export LOADEDMODULES=$LOADEDMODULES:opt_local/1.0:gcc/4.8.0:python/3.4.0
+export _LMFILES_=$_LMFILES_:/afs/crc.nd.edu/x86_64_linux/Modules/modules/development_tools_and_libraries/python/3.4.0
+export PATH=$PATH:/afs/crc.nd.edu/x86_64_linux/python/3.4.0/gcc-4.8.0/bin
 
 /afs/crc.nd.edu/user/c/ckankel/Public/node_search/node_search.py $@
 
